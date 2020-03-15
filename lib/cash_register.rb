2936ea12695c5833021ffe89c7556cd56867a64b
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-15 20:26:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+ 
+class CashRegister 
+  attr_accessor :discount, :item, :last_transaction, :total
 
+  def initialize(discount = 0)
+    @items = []
+    @discount = discount
+    @total = 0 
+  end 
+  
+  def add_item(cost, name, number = 1)
+    self.total = cost * number
+    @items << name
+  end
+  
+  def add_item_2(cost, name, number = 1)
+    self.total = cost * number
+
+def items 
+  end
+  
+  def void_last_transaction
+  end 
+  end
